25de81e09ece0617911ead4f94d5c3c11cc390df
litingyes <luz.liting@gmail.com>
2025-01-18 10:12:54 +0800

    refactor(export): adjust logic

--- scripts/export.ts
+++ scripts/export.ts
@@ -2,63 +2,11 @@ import { writeFileSync } from 'node:fs'
 import { basename, resolve } from 'node:path'
 import { globSync } from 'glob'
 
-function exportFunctionModule() {
-  const dir = resolve(__dirname, '../src/function')
-  const files = globSync('**/*.ts', { cwd: dir, ignore: ['index.ts'] })
-
-  let code = `/**
- * @module
- */\n\n`
-  files.forEach((file) => {
-    code += `export * from './${basename(file, '.ts')}'\n`
-  })
-  writeFileSync(resolve(dir, 'index.ts'), code)
-}
-
-function exportIsModule() {
-  const dir = resolve(__dirname, '../src/is')
-  const files = globSync('**/*.ts', { cwd: dir, ignore: ['index.ts'] })
-
-  let code = `/**
- * @module
- */\n\n`
-  files.forEach((file) => {
-    code += `export * from './${basename(file, '.ts')}'\n`
-  })
-  writeFileSync(resolve(dir, 'index.ts'), code)
-}
-
-function exportStringModule() {
-  const dir = resolve(__dirname, '../src/string')
-  const files = globSync('**/*.ts', { cwd: dir, ignore: ['index.ts'] })
-
-  let code = `/**
- * @module
- */\n\n`
-  files.forEach((file) => {
-    code += `export * from './${basename(file, '.ts')}'\n`
-  })
-  writeFileSync(resolve(dir, 'index.ts'), code)
-}
-
-function exportObjectModule() {
-  const dir = resolve(__dirname, '../src/object')
-  const files = globSync('**/*.ts', { cwd: dir, ignore: ['index.ts'] })
-
-  let code = `/**
- * @module
- */\n\n`
-  files.forEach((file) => {
-    code += `export * from './${basename(file, '.ts')}'\n`
-  })
-  writeFileSync(resolve(dir, 'index.ts'), code)
-}
-
 function exportModules() {
   const dir = resolve(__dirname, '../src')
   const modules = globSync('*/', { cwd: dir })
 
-  let code = `/**
+  let mainModuleCode = `/**
  * A tool kit for web development.
  *
  * ## Installation
@@ -69,17 +17,22 @@ function exportModules() {
  * @module
  */\n\n`
   modules.forEach((module) => {
-    code += `export * from './${module}'\n`
+    mainModuleCode += `export * from './${module}'\n`
+
+    const moduleDir = resolve(dir, module)
+    const moduleFiles = globSync('**/*.ts', { cwd: moduleDir, ignore: ['index.ts'] })
+    let moduleCode = `/**
+    * @module
+    */\n\n`
+    moduleFiles.forEach((file) => {
+      moduleCode += `export * from './${basename(file, '.ts')}'\n`
+    })
+    writeFileSync(resolve(moduleDir, 'index.ts'), moduleCode)
   })
-  writeFileSync(resolve(dir, 'index.ts'), code)
+  writeFileSync(resolve(dir, 'index.ts'), mainModuleCode)
 }
 
 function main() {
-  exportFunctionModule()
-  exportIsModule()
-  exportStringModule()
-  exportObjectModule()
-
   exportModules()
 }
 
